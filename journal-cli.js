c42c05b86203c1c1b3021aeadad6a52a601d3e9d
Robert Wettlaufer <rdwettlaufer@gmail.com>
2014-07-21 16:08:53 +0000

    Basic tag support

--- journal-cli.js
+++ journal-cli.js
@@ -1,4 +1,5 @@
 var config   = require('./config.js').config
+  , program  = require('commander')
   , Firebase = require('firebase')
   , baseRef  = new Firebase(config.firebase)
   , _        = require('underscore')
@@ -6,24 +7,56 @@ var config   = require('./config.js').config
   , tags     = {};
 
 
+program
+  .version('0.0.1')
+  .option('-n, --number', 'See the last [number] of entries')
+  .option('-t, --tag'   , 'See entries containing [tag]')
+  .parse(process.argv);
+
+if (program.number) {
+  getEntries();
+} else if (program.tag) {
+  getTags();
+} else {
+  parseEntry(process.argv[2]);
+}
+
+
+function getEntries(num) {
+  num = num || 10;
+  var entryQuery = entryRef.limit(num).once('value', function(snap) {
+    _.each(snap.val(), function(entry) {
+      console.log(entry.body);
+    });
+  });
+}
+
+function getTags(num) {
+  num = num || 10;
+  var entryQuery = baseRef.child('tags').limit(num).once('value', function(snap) {
+    _.each(snap.val(), function(entry, key, list) {
+      _.each(entry, function(entryVal) {
+        console.log(key + ' -> ' + entryVal);
+      });
+    });
+  });
+}
+
 function parseEntry(line) {
   if (!line) return;
 
-  var words     = line.split('')
+  var words     = line.split(' ')
     , tagQueue  = []
-    , wordQueue = []
-    , tagHolder = {}
     // jrnl does some basic time-stamping keywords like 'yesterday' or
     // '3 pm' that resolves to a timestamp. Not hard, can do that later.
     , time      = Date.now()
     , tagRef;
 
   _.each(words, function(word) {
-    if (word[0] === '@' && word[1]) {
+    if (word[0] === '@' && typeof word[1] !== "undefined") {
       // Create an array with the tag as the key, and sentence as the value
-      tagQueue.push([word.slice(1), words]);
+      tagQueue.push([word.slice(1), line]);
     }
-    wordQueue.push(word);
   });
 
   // Build the hash/object for tags
@@ -36,14 +69,12 @@ function parseEntry(line) {
     }
   });
 
-  entryRef.push({body: line, timestamp: Date.now()});
-
-  /*
-  _.each(tagQueue, function(sentence, tag) {
-    tagRef = new Firebase(config.firebase + '/' + tag);
-    tagRef.push(sentence);
+  _.each(tagQueue, function(tagEntry) {
+    tagRef = new Firebase(config.firebase + '/tags/');
+    tagRef = tagRef.child(tagEntry[0]);
+    tagRef.push(tagEntry[1]);
   });
-  */
+
+  entryRef.push({body: line, timestamp: Date.now()});
 };
 
-parseEntry(process.argv[2]);
