d6fd22afb9d48088b4e7ac343ba909d3d2828747
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-07-22 16:48:47 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,6 @@ var myApp = angular.module("MyJournal", ['ngAnimate', 'firebase']);
 
 function JournalController($scope, $firebase) {
   var entryRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/entries");
-  var topRef = new Firebase("https://sweltering-fire-8827.firebaseio.com");
-  var auth = new FirebaseSimpleLogin(topRef, function(error, user) {
-      if (error) console.log(error)
-      console.log(user);
-  });
   //var tagRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/tags");
   // Automatically syncs everywhere in realtime
   $scope.entries = $firebase(entryRef);
@@ -15,5 +10,4 @@ function JournalController($scope, $firebase) {
   $scope.entries.$on('loaded', function() {
     $scope.showLoading = false;
   });
-  auth.login('github');
 }
