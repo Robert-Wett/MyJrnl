2c29706dc7f68cacd987aabaf8d168788e92d958
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-07-21 21:28:56 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+var journalApp = angular.module("MyJournal", ['ngAnimate', 'firebase']);
+
+function JournalController($scope, $firebase) {
+  var entryRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/entries");
+  var tagRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/tags");
+  // Automatically syncs everywhere in realtime
+  $scope.entries = $firebase(entryRef);
+  $scope.userTags = $firebase(tagRef);
+}
