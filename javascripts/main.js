9bea5073c9407405c99e7d7cff2206e83de4666f
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-07-23 14:27:08 -0400

    add ngSanitize/linky

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,8 @@
-var myApp = angular.module("MyJournal", ['ngAnimate', 'firebase']);
+var myApp = angular.module("MyJournal", ['ngAnimate', 'firebase', 'ngSanitize']);
 
 function JournalController($scope, $firebase) {
   var entryRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/entries");
   //var tagRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/tags");
-  // Automatically syncs everywhere in realtime
   $scope.entries = $firebase(entryRef);
   //$scope.userTags = $firebase(tagRef);
   $scope.showLoading = true;
