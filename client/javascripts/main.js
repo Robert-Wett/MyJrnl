a8119783cc003fa98c765963b34ecc3a373076d3
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-08-09 02:39:58 +0000

    consolidating gh-pages branch

---
+++ client/javascripts/main.js
@@ -0,0 +1,12 @@
+var myApp = angular.module("MyJournal", ['ngAnimate', 'firebase', 'ngSanitize']);
+
+function JournalController($scope, $firebase) {
+  var entryRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/entries");
+  //var tagRef = new Firebase("https://sweltering-fire-8827.firebaseio.com/tags");
+  $scope.entries = $firebase(entryRef);
+  //$scope.userTags = $firebase(tagRef);
+  $scope.showLoading = true;
+  $scope.entries.$on('loaded', function() {
+    $scope.showLoading = false;
+  });
+}
